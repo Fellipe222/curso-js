ba1f7f5946ad9d7fd3dd2ff886c831ef2421d8bb
barrosf <fellipe.engmec@gmail.com>
2020-06-06 22:16:38 -0300

    Script com exemplo de utilização de if/else retornando uma string, de acordo com a versçao mais atualizada do ES6.

---
+++ ex009.js
@@ -0,0 +1,12 @@
+var pais = 'EUA'
+var nacionalidade = null
+
+if (pais == 'Brasil'){
+    //console.log('Você é brasileiro')
+    nacionalidade = 'você é brasileiro'
+}else{
+    //console.log('Você é estrangeiro')
+    nacionalidade = 'você é estrangeiro!'
+}
+
+console.log(`Vivendo em ${pais}, ${nacionalidade}!`)
