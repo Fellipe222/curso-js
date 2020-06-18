3e3166213d60b54e5a28714e02abcb30d1f1ef41
barrosf <fellipe.engmec@gmail.com>
2020-06-17 23:09:57 -0300

    Mais exemplos de utilização: "while", "do while" e "for"

---
+++ ex017.js
@@ -0,0 +1,16 @@
+var a = 0
+for (a == 0 ; a <= 10 ; a ++){
+    console.log(`a${a}`)
+}
+
+var b = 0
+while (b <= 10){
+    console.log(`b${b}`)
+    b ++
+}
+
+var c = 0
+do {
+    console.log(`c${c}`)
+    c ++
+}while (c <= 10)
