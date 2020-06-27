f15e5bd9af68970e77dd16f7a3037a9c9ab6804b
barrosf <fellipe.engmec@gmail.com>
2020-06-26 22:43:07 -0300

    Intro à funções parametrizadas em JS

---
+++ ex022.js
@@ -0,0 +1,9 @@
+// Parâmetros de funções
+
+function soma (n1=0 , n2=0){ 
+    /* Igualamos o valor de n1 e n2 a zero, pois se caso o usuário
+     não dê entrada em algum dos valores, o zero será considerado*/
+    return n1 + n2
+}
+
+console.log(soma(2,5))
